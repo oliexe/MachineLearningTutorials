49b55fb73980b522b60f86dc0c17fb8b532760e4
Ondřej Řeháček <Ondrejrehacek@gmail.com>
2016-10-25 11:56:08 +0200

    k-means implementation

---
+++ iris.dataset/MAD1_cv2/Program.cs
@@ -0,0 +1,446 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.Axes;
+
+namespace MAD1_cv2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new StreamReader(File.OpenRead(@"C:\iris.csv"));
+            List<iris> list = new List<iris>();
+            reader.ReadLine();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                line = line.Replace(",", ".");
+                var values = line.Split(';');
+                iris result_line = new iris(Convert.ToDouble(values[0]), Convert.ToDouble(values[1]), Convert.ToDouble(values[2]), Convert.ToDouble(values[3]), values[4]);
+                list.Add(result_line);
+            }
+
+            List<double> sepallen_list = new List<double>();
+            List<double> sepalwid_list = new List<double>();
+            List<double> petallen_list = new List<double>();
+            List<double> petalwid_list = new List<double>();
+
+            foreach (iris line in list)
+            {
+                sepallen_list.Add(line.sepallen);
+                sepalwid_list.Add(line.sepalwid);
+                petallen_list.Add(line.petallen);
+                petalwid_list.Add(line.petalwid);
+            }
+
+            System.IO.Directory.CreateDirectory("sepal_lenght");
+            System.IO.Directory.CreateDirectory("sepal_width");
+            System.IO.Directory.CreateDirectory("petal_lenght");
+            System.IO.Directory.CreateDirectory("petal_width");
+
+            Console.WriteLine("SEPAL LENGTH");           
+            Console.WriteLine("Average: " + GetAverage(sepallen_list));
+            Console.WriteLine("Variance: " + GetVariance(sepallen_list));
+            Console.WriteLine("Deviation: " + GetStdDev(sepallen_list));
+            Console.WriteLine("Median: " + GetMedian(sepallen_list));
+            isNoramalDistribution(sepallen_list, GetAverage(sepallen_list), GetStdDev(petalwid_list));
+            Console.WriteLine();
+
+            Console.WriteLine("SEPAL WIDTH");
+            Console.WriteLine("Average: " + GetAverage(sepalwid_list));
+            Console.WriteLine("Variance: " + GetVariance(sepalwid_list));
+            Console.WriteLine("Deviation: " + GetStdDev(sepalwid_list));
+            Console.WriteLine("Median: " + GetMedian(sepalwid_list));
+            isNoramalDistribution(sepalwid_list, GetAverage(sepalwid_list), GetStdDev(sepalwid_list));
+            Console.WriteLine();
+
+            Console.WriteLine("PETAL LENGTH");
+            Console.WriteLine("Average: " + GetAverage(petallen_list));
+            Console.WriteLine("Variance: " + GetVariance(petallen_list));
+            Console.WriteLine("Deviation: " + GetStdDev(petallen_list));
+            Console.WriteLine("Median: " + GetMedian(petallen_list));
+            isNoramalDistribution(petallen_list, GetAverage(petallen_list), GetStdDev(petallen_list));
+            Console.WriteLine();
+
+            Console.WriteLine("PETAL WIDTH");
+            Console.WriteLine("Average: " + GetAverage(petalwid_list));
+            Console.WriteLine("Variance: " + GetVariance(petalwid_list));
+            Console.WriteLine("Deviation: " + GetStdDev(petalwid_list));
+            Console.WriteLine("Median: " + GetMedian(petalwid_list));
+            isNoramalDistribution(petalwid_list, GetAverage(petalwid_list), GetStdDev(petalwid_list));
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadKey();
+
+            double[,] euclid = GenerateEuclid(list);
+            CSVgenerator(euclid, "euclid_distance.csv");
+
+            double[,] cosine = GenerateCosine(list);
+            CSVgenerator(cosine, "cosine_similarity.csv");
+
+            Occurence(sepallen_list, "sepal_lenght/occurences.csv");
+            Occurence(sepalwid_list, "sepal_width/occurences.csv");
+            Occurence(petallen_list, "petal_lenght/occurences.csv");
+            Occurence(petalwid_list, "petal_width/occurences.csv");
+
+            MakeGraphs(sepallen_list, "sepal_lenght/graph");
+            MakeGraphs(sepalwid_list, "sepal_width/graph");
+            MakeGraphs(petallen_list, "petal_lenght/graph");
+            MakeGraphs(petalwid_list, "petal_width/graph");
+
+            Console.ReadKey();
+
+            kmeans k_means = new kmeans();
+            k_means.InitData(petalwid_list, petallen_list);
+            k_means.InitData(sepalwid_list, sepallen_list);
+            k_means.Execute(6, true);
+
+            Console.ReadKey();
+        }
+
+        //Median (na double poli)
+        public static double GetMedian(List<double> source)
+        {
+            double[] sourceNumbers = source.ToArray();
+            double[] sortedPNumbers = (double[])sourceNumbers.Clone();
+            Array.Sort(sortedPNumbers);
+            int size = sortedPNumbers.Length;
+            int mid = size / 2;
+            double median = (size % 2 != 0) ? (double)sortedPNumbers[mid] : ((double)sortedPNumbers[mid] + (double)sortedPNumbers[mid - 1]) / 2;
+            return median;
+        }
+
+        //Pruměr (na double poli)
+        public static double GetAverage(List<double> source)
+        {
+            double[] sourceNumbers = source.ToArray();
+            return sourceNumbers.Take(sourceNumbers.Count()).Average();
+        }
+
+        //Standartní odchylka (na double poli)
+        public static double GetStdDev(List<double> source)
+        {
+            double ret = 0;
+            if (source.Count() > 0)
+            {    
+                double avg = GetAverage(source);     
+                double sum = source.Sum(d => Math.Pow(d - avg, 2));    
+                ret = Math.Sqrt((sum) / (source.Count() - 1));
+            }
+            return ret;
+        }
+
+        //Rozptyl (na double poli)
+        public static double GetVariance(List<double> source)
+        {
+            double variance = 0;
+
+            for (int i = 0; i < source.Count; i++)
+            {
+                variance += Math.Pow((source[i] - GetAverage(source)), 2);
+            }
+
+            return variance / (source.Count);
+        }
+
+        //Euklidovská vzdálenost
+        public static double GetEuclidDist(iris a, iris b)
+        {
+            double dist = Math.Sqrt(((a.sepallen - b.sepallen) *  (a.sepallen - b.sepallen) + 
+                (a.sepalwid - b.sepalwid) * (a.sepalwid - b.sepalwid) +
+                (a.petallen - b.petallen) * (a.petallen - b.petallen) +
+                (a.petalwid - b.petalwid) * (a.petalwid - b.petalwid)
+                ));
+            return dist;
+        }
+
+        //https://bioinformatics.oxfordjournals.org/content/suppl/2009/10/24/btp613.DC1/bioinf-2008-1835-File004.pdf
+        //Cosinova podobnost
+        public static double GetCosineDist(iris a, iris b)
+        {
+            double dist1 = (a.sepallen * b.sepallen) + (a.sepalwid * b.sepalwid) + (a.petallen * b.petallen) + (a.petalwid * b.petalwid);
+            double dist2 = Math.Sqrt(
+                (a.sepallen * a.sepallen) + 
+                (a.sepalwid * a.sepalwid) + 
+                (a.petallen * a.petallen) + 
+                (a.petalwid * a.petalwid)
+                ) * Math.Sqrt(
+                (b.sepallen * b.sepallen) +
+                (b.sepalwid * b.sepalwid) +
+                (b.petallen * b.petallen) +
+                (b.petalwid * b.petalwid)
+                );
+
+            return dist1 / dist2;
+        }
+
+        //Generovat euklidovské vzalenosti pro cely dataset
+        public static double[,] GenerateEuclid(List<iris> source)
+        {
+
+            double[,] x = new double[source.Count,source.Count];
+
+            for (int i = 0; i < source.Count(); i++)
+                {
+                    for (int z = 0; z < source.Count(); z++)
+                    {
+                    x[i,z] = GetEuclidDist(source[i], source[z]);
+                    }
+                }
+            return x;
+        }
+
+        //Generovat cos podobnost pro cely dataset
+        public static double[,] GenerateCosine(List<iris> source)
+        {
+
+            double[,] x = new double[source.Count, source.Count];
+
+            for (int i = 0; i < source.Count(); i++)
+            {
+                for (int z = 0; z < source.Count(); z++)
+                {
+                    x[i, z] = GetCosineDist(source[i], source[z]);
+                }
+            }
+            return x;
+        }
+
+        //CV4
+
+        //četnost - relativni+kumulativni jednotlivých hodnot atributů
+        public static void Occurence(List<double> source , string filename)
+        {
+            int count = 0;
+            List<double> workData = new List<double>();
+
+            foreach (var petal in source)
+            {
+                workData.Add(petal);
+            }
+
+         
+            using (StreamWriter writer =
+        new StreamWriter(filename))
+            {
+                //Rozdělení hodnot do grup podle hodnot
+                workData.Sort();
+                var groups = workData.GroupBy(i => i);
+
+            writer.WriteLine("Hodnota,Pocet Vyskytu,Kumulativni cetnost,Relativni cetnost,CDF,PDF");
+                //Sečist vyskyty v jednotlivých grupách
+
+                foreach (var iris in groups)
+            {
+                writer.Write("{0},{1},", iris.Key, iris.Count());
+                count = count + iris.Count();
+                writer.Write(count+",");
+                writer.Write(count / 1.5 + ",");
+                writer.Write(iris.Count() / 1.5 + ",");
+                writer.Write(PDFfce(StandartDistribution(iris.Key, GetAverage(workData), GetStdDev(workData))));
+                writer.WriteLine();
+            }
+            }
+            Console.WriteLine(filename + " generated!");
+        }
+
+        //Je normální distribucí ?
+        public static void isNoramalDistribution(List<double> source, double mean, double deviation)
+        {
+            int cOfIsTrue = 0;
+            double p1 = mean - deviation;
+            double p2 = mean + deviation;
+            double numberOfSatisElem = 0;
+            double[] array = source.ToArray();
+
+            // 1 * o 
+            for (int i = 0; i < array.Length; i++)
+            {
+                if ((array[i] >= p1) && (array[i] <= p2))
+                {
+                    numberOfSatisElem++;
+                }
+            }
+
+            if (numberOfSatisElem >= ((array.Length / 100.0) * 68.0))
+            {
+                cOfIsTrue++;
+            }
+
+            numberOfSatisElem = 0;
+            p1 = mean - 2 * deviation;
+            p2 = mean + 2 * deviation;
+
+            // 2 * o
+            for (int i = 0; i < array.Length; i++)
+            {
+                if ((array[i] >= p1) && (array[i] <= p2))
+                {
+                    numberOfSatisElem++;
+                }
+            }
+
+            if (numberOfSatisElem >= ((array.Length / 100.0) * 95.0))
+            {
+                cOfIsTrue++;
+            }
+
+            numberOfSatisElem = 0;
+            p1 = mean - 3 * deviation;
+            p2 = mean + 3 * deviation;
+
+            // 3 * o
+            for (int i = 0; i < array.Length; i++)
+            {
+                if ((array[i] >= p1) && (array[i] <= p2))
+                {
+                    numberOfSatisElem++;
+                }
+            }
+
+            if (numberOfSatisElem >= ((array.Length / 100.0) * 99.7))
+            {
+                cOfIsTrue++;
+            }
+
+            if (cOfIsTrue == 3)
+            {
+                Console.WriteLine("Je normalne rozlozeno");
+            }
+            else
+            {
+                Console.WriteLine("!! Neni normalne rozlozeno !!");
+            }
+        }
+
+        //Výpočet CDF
+        public static double StandartDistribution(double score, double average, double standardDeviation)
+        {
+            if (standardDeviation == 0) return 0;
+
+            return (score - average) / standardDeviation;
+        }
+
+        //Výpočet PDF
+        public static double PDFfce(double StandartDistribution)
+        {
+            var exponent = -1 * (0.5 * Math.Pow(StandartDistribution, 2));
+            var numerator = Math.Pow(Math.E, exponent);
+            var denominator = Math.Sqrt(2 * Math.PI);
+            return numerator / denominator;
+        }
+
+        //Generovat grafy pro atribut
+        public static void MakeGraphs(List<double> source, string filename)
+        {
+            int count = 0;
+            PlotModel graf = new PlotModel { Title = "Histogram " + filename };
+            PlotModel graf_cdf = new PlotModel { Title = "CDF " + filename };
+            PlotModel graf_pdf = new PlotModel { Title = "PDF " + filename };
+
+            List<double> workData = new List<double>();
+
+            foreach (var petal in workData)
+            {
+                workData.Add(petal);
+            }
+
+
+            workData.Sort();
+
+            double[] values = workData.ToArray();
+            var bucketeer = new Dictionary<double, double>();
+            var groups = workData.GroupBy(i => i);
+            List<string> kek = new List<string>();
+
+            var overlayData = new LineSeries();
+            var PDF = new LineSeries();
+
+            int z = 0;
+            foreach (var iris in groups)
+            {
+                bucketeer.Add(iris.Key, iris.Count());
+                kek.Add(iris.Key.ToString());
+                count = count + iris.Count();
+                z++;
+
+                overlayData.Points.Add(new DataPoint(z, count / 1.5));
+                PDF.Points.Add(new DataPoint(z, PDFfce(StandartDistribution(iris.Key, GetAverage(workData), GetStdDev(workData)))));
+            }
+
+            ColumnSeries ColSer = new ColumnSeries();
+            CategoryAxis Axis = new CategoryAxis();
+            
+            foreach (var pair in bucketeer.OrderBy(x => x.Key))
+            {
+                ColSer.Items.Add(new ColumnItem(pair.Value));                
+            }
+
+            Axis.ItemsSource = kek;
+            CategoryAxis Axis2 = new CategoryAxis();
+            Axis2.ItemsSource = kek;
+            CategoryAxis Axis3 = new CategoryAxis();
+            Axis3.ItemsSource = kek;
+
+            graf.Series.Add(ColSer);
+            graf.Axes.Add(Axis);
+
+            graf_cdf.Series.Add(overlayData);
+            graf_cdf.Axes.Add(Axis2);
+
+            graf_pdf.Series.Add(PDF);
+            graf_pdf.Axes.Add(Axis3);
+
+
+            using (var stream = File.Create(filename + "_histogram.pdf"))
+            {
+                var pdfExporter = new PdfExporter { Width = 1000, Height = 400 };
+                pdfExporter.Export(graf, stream);
+                Console.WriteLine("Graph " + filename + "_histogram.pdf" + " generated!");
+            }
+
+            using (var stream = File.Create(filename + "_CDF.pdf"))
+            {
+                var pdfExporter = new PdfExporter { Width = 1000, Height = 400 };
+                pdfExporter.Export(graf_cdf, stream);
+                Console.WriteLine("Graph " + filename + "_CDF.pdf" + " generated!");
+            }
+
+            using (var stream = File.Create(filename + "_PDF.pdf"))
+            {
+                var pdfExporter = new PdfExporter { Width = 1000, Height = 400 };
+                pdfExporter.Export(graf_pdf, stream);
+                Console.WriteLine("Graph " + filename + "_PDF.pdf" + " generated!");
+            }
+
+        }
+
+        //zapis do CSV
+        public static void CSVgenerator(double[,] source, string filename)
+        {
+            using (StreamWriter writer =
+            new StreamWriter(filename))
+            {
+                double[,] x = source;
+
+                for (int i = 0; i < 150; i++)
+                {
+                    for (int z = 0; z < 150; z++)
+                    {
+                        writer.Write(x[i, z] + ",");
+                    }
+                    writer.WriteLine();
+                }
+            }
+            Console.WriteLine(filename + " generated!");
+        }
+    }
+}
---
+++ iris.dataset/MAD1_cv2/kmeans.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.Axes;
+using System.IO;
+
+namespace MAD1_cv2
+{
+    public class kmeans
+    {
+        List<meansPoint> _rawDataToCluster = new List<meansPoint>();
+        List<meansPoint> _normalizedDataToCluster = new List<meansPoint>();
+        List<meansPoint> _clusters = new List<meansPoint>();
+        private int _numberOfClusters = 0;
+        PlotModel graf = new PlotModel();
+
+        //Grafování rozložení x,y
+        public void AddToGraph(List<double> x, List<double> y, byte r, byte g, byte b)
+        {
+            var scatterSeries = new ScatterSeries { MarkerType = MarkerType.Cross, MarkerStroke = OxyColor.FromRgb(r, g, b) };
+
+            for (int i = 0; i < x.Count; i++)
+            {
+                scatterSeries.Points.Add(new ScatterPoint(x[i], y[i]));
+            }
+
+            graf.Series.Add(scatterSeries);
+        }
+
+        public void GenerateGraph()
+        {
+            using (var stream = File.Create("kmeans.pdf"))
+            {
+                var pdfExporter = new PdfExporter { Width = 1000, Height = 400 };
+                pdfExporter.Export(graf, stream);
+                Console.WriteLine("k-means graph generated!");
+            }
+        }
+
+        public byte[] GetRandomColor()
+        {
+            Random r = new Random(DateTime.Now.Millisecond);
+
+            return new byte[] { (byte)r.Next(0, 255), (byte)r.Next(0, 255), (byte)r.Next(0, 255) };
+        }
+
+        //Přidat data (Width X Lenght) do k-means
+        public void InitData(List<double> Width, List<double> Lenght)
+        {
+            for (int i = 0; i < Lenght.Count(); i++)
+            {
+                _rawDataToCluster.Add(new meansPoint(Width[i], Lenght[i]));
+            }
+        }
+
+        //Normalizace bodů
+        private void NormalizeData()
+        {
+            double widthSum = 0.0;
+            double lengthSum = 0.0;
+
+            foreach (meansPoint dataPoint in _rawDataToCluster)
+            {
+                widthSum += dataPoint.Width;
+                lengthSum += dataPoint.Length;
+            }
+
+            double widthMean = widthSum / _rawDataToCluster.Count;
+            double lengthMean = lengthSum / _rawDataToCluster.Count;
+
+            double sumWidth = 0.0;
+            double sumLength = 0.0;
+            foreach (meansPoint dataPoint in _rawDataToCluster)
+            {
+                sumWidth += Math.Pow(dataPoint.Width - widthMean, 2);
+                sumLength += Math.Pow(dataPoint.Length- lengthMean, 2);
+            }
+
+            double widthSD = sumWidth / _rawDataToCluster.Count;
+            double lengthSD = sumLength / _rawDataToCluster.Count;        
+            foreach (meansPoint dataPoint in _rawDataToCluster)
+            {
+                _normalizedDataToCluster.Add(new meansPoint()
+                {
+                    Width = (dataPoint.Width - widthMean) / widthSD,
+                    Length = (dataPoint.Length - lengthMean) / lengthSD
+                }
+                );
+            }
+        }
+
+        //Nastavit počet clusetru
+        private void SetClusters(int num)
+        {
+            _numberOfClusters = num;
+        }
+
+        //Helper.: Test zda má cluster alespoň jeden přiřazený bod
+        private bool IsClusterEmpty(List<meansPoint> data)
+        {
+            var emptyCluster =
+            data.GroupBy(s => s.Cluster).OrderBy(s => s.Key).Select(g => new { Cluster = g.Key, Count = g.Count() });
+
+            foreach (var item in emptyCluster)
+            {
+                if (item.Count == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //Inicializovat centroidy náhodně
+        private void InitializeCentroids()
+        {
+            Random random = new Random(_numberOfClusters);
+
+            //Přiřadime nahodný DataPoint zvolenému clusteru (Aspon jeden centroid má jeden DatatPoint)
+            for (int i = 0; i < _numberOfClusters; ++i)
+            {
+                _normalizedDataToCluster[i].Cluster = _rawDataToCluster[i].Cluster = i;
+            }
+
+            //Zbytku DataPointu přiřadíme náhodné centroidy 
+            //Nějaké "aribtary clusters" u kterých dojde k přehodnocení
+            for (int i = _numberOfClusters; i < _normalizedDataToCluster.Count; i++)
+            {
+                _normalizedDataToCluster[i].Cluster = _rawDataToCluster[i].Cluster = random.Next(0, _numberOfClusters);
+            }
+        }
+
+        //Nalezení nejvhodnějšího centroidu
+        //Počítáme průměr pro cluster
+        private bool UpdateDataPointMeans()
+        {
+            if (IsClusterEmpty(_normalizedDataToCluster)) return false;
+
+            var groupToComputeMeans = _normalizedDataToCluster.GroupBy(s => s.Cluster).OrderBy(s => s.Key);
+
+            int clusterIndex = 0;
+            double width = 0.0;
+            double length = 0.0;
+
+            foreach (var item in groupToComputeMeans)
+            {
+                foreach (var value in item)
+                {
+                    width += value.Width;
+                    length += value.Length;
+                }
+
+                _clusters[clusterIndex].Width = width / item.Count();
+                _clusters[clusterIndex].Length = length / item.Count();
+
+                clusterIndex++;
+                width = 0.0;
+                length = 0.0;
+            }
+            return true;
+        }
+
+        //Výpočet euclidovy vzdálenosti
+        private double ElucidanDistance(meansPoint dataPoint, meansPoint mean)
+        {
+            double _diffs = 0.0;
+            _diffs = Math.Pow(dataPoint.Width - mean.Width, 2);
+            _diffs += Math.Pow(dataPoint.Length - mean.Length, 2);
+            return Math.Sqrt(_diffs);
+        }
+
+        //Přesunout MeansPointy do správných clusterů
+        private bool UpdateClusterMembership()
+        {
+            bool changed = false;
+
+            double[] distances = new double[_numberOfClusters];
+
+            for (int i = 0; i < _normalizedDataToCluster.Count; ++i)
+            {
+
+                for (int k = 0; k < _numberOfClusters; ++k)
+                    distances[k] = ElucidanDistance(_normalizedDataToCluster[i], _clusters[k]);
+
+                int newClusterId = MinIndex(distances);
+                if (newClusterId != _normalizedDataToCluster[i].Cluster)
+                {
+                    changed = true;
+                    _normalizedDataToCluster[i].Cluster = _rawDataToCluster[i].Cluster = newClusterId;
+
+                    Console.WriteLine("Data Point >> Width: " + _rawDataToCluster[i].Width + ", Lenght: " +
+                    _rawDataToCluster[i].Length + " moved to Cluster # " + newClusterId);
+                }
+            }
+            if (changed == false)
+                return false;
+            if (IsClusterEmpty(_normalizedDataToCluster)) return false;
+            return true;
+        }
+
+        //Helper.: Nejmenší hodnota v poli
+        private int MinIndex(double[] distances)
+        {
+            int _indexOfMin = 0;
+            double _smallDist = distances[0];
+            for (int k = 0; k < distances.Length; ++k)
+            {
+                if (distances[k] < _smallDist)
+                {
+                    _smallDist = distances[k];
+                    _indexOfMin = k;
+                }
+            }
+            return _indexOfMin;
+        }
+
+        //Spustit k-means
+        public void Execute(int NumberOfClusters, bool print)
+        {
+            Console.WriteLine();
+            Console.WriteLine("K-means...");
+            SetClusters(NumberOfClusters);
+
+            for (int i = 0; i < _numberOfClusters; i++)
+            {
+                _clusters.Add(new meansPoint() { Cluster = i });
+            }
+
+            bool _changed = true;
+            bool _success = true;
+
+            NormalizeData();
+            InitializeCentroids();
+            
+
+            int maxIteration = _normalizedDataToCluster.Count * 10;
+            int _threshold = 0;
+            while (_success == true && _changed == true && _threshold < maxIteration)
+            {
+                ++_threshold;
+                _success = UpdateDataPointMeans();
+                _changed = UpdateClusterMembership();
+            }
+
+            if (print)
+            {
+                var group = _rawDataToCluster.GroupBy(s => s.Cluster).OrderBy(s => s.Key);
+
+                foreach (var g in group)
+                {
+                    Console.WriteLine("Cluster # " + g.Key + ":");
+
+                    List<double> Wid = new List<double>();
+                    List<double> Len = new List<double>();
+
+
+                    foreach (var value in g)
+                    {
+                        Console.WriteLine(value.ToString());
+                        Wid.Add(value.Width);
+                        Len.Add(value.Length);
+                    }
+
+                    byte[] color = GetRandomColor();
+
+                    AddToGraph(Wid, Len, color[0], color[1], color[2]);
+                    Console.WriteLine("------------------------------");
+                }
+                GenerateGraph();
+            }
+        }
+        
+    }
+
+
+
+    public class meansPoint
+    {
+        public double Width { get; set; }
+        public double Length { get; set; }
+        public int Cluster { get; set; }
+        public meansPoint(double width, double lenght)
+        {
+            Width = width;
+            Length = lenght;
+            Cluster = 0;
+        }
+
+        public meansPoint()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{{{0},{1}}}", Width.ToString("f" + 1), Length.ToString("f" + 1));
+        }
+    }
+
+}
