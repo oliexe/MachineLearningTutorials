eab1644ad29d82a1170ad7dc1a71e2bbad8c1470
Ondřej Řeháček <Ondrejrehacek@gmail.com>
2016-11-25 11:36:06 +0100

    Homework

---
+++ Homework/Data/Videogame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework.Data
+{
+    class Videogame
+    {
+        public int rank;
+        public string name;
+        public string platform;
+        public int year;
+        public string genre;
+        public string publisher;
+        public double naSales;
+        public double euSales;
+        public double jpSales;
+        public double otherSales;
+        public double globalSales;
+
+        public Videogame(int rank, string name, string platform, int year, string genre, string publisher,
+            double naSales, double euSales, double jpSales, double otherSales, double globalSales)
+        {
+            this.rank = rank;
+            this.name = name;
+            this.platform = platform;
+            this.year = year;
+            this.genre = genre;
+            this.publisher = publisher;
+            this.naSales = naSales;
+            this.euSales = euSales;
+            this.jpSales = jpSales;
+            this.otherSales = otherSales;
+            this.globalSales = globalSales;
+        }
+    }
+}
---
+++ Homework/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Homework.Data;
+using Homework.Helpers;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loader load = new Loader();
+            List<Videogame> data = load.csv(@"C:\vgsales.csv");
+        }
+    }
+}
