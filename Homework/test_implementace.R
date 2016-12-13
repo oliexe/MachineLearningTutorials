a5e887636e1aa138d0434152f5f24240314e2a3b
Ondřej Řeháček <Ondrejrehacek@gmail.com>
2016-12-13 14:05:10 +0100

    Homework finalization

---
+++ Homework/test_implementace.R
@@ -0,0 +1,12 @@
+
+library("dbscan")
+data <- read.csv(file = "c:/vgsales.csv")[ ,c('EU_Sales', 'NA_Sales')]
+
+x <- as.matrix(data[, 1:2])
+
+## DBSCAN
+db <- dbscan(x, eps = 1.0, minPts = 2)
+db
+
+
+pairs(x, col = db$cluster + 1L)
