f1b0171c47ea933c619cfc7c7c72ef01ddabeefa
Ondřej Řeháček <Ondrejrehacek@gmail.com>
2017-10-02 14:18:10 +0200

    MAD3 - Excercise 1

--- Experiments/mad3_cv1/mad3_cv1/script.R
+++ Experiments/mad3_cv1/mad3_cv1/script.R
@@ -1,19 +1,20 @@
 ## MAD 3 - Cvièení 1
 ## Ondøej Øeháèek - REH0063
 
-## Instalovat igrapgh package
 install.packages("igraph")
-
-## igraph
 library(igraph)
 
-## 1a) Random graph
-rand <- sample_gnm(n = 100, m = 250)
-plot(rand, vertex.size = 6, vertex.label = NA)
+## --------------------1--------------------
 
-## 1b) Barabási–Albert model
-bara <- make_tree(40, children = 3, mode = "undirected")
-plot(bara, vertex.size = 10, vertex.label = NA)
+## 1a) Random graph + plot
+rand <- sample_gnm(n = 200, m = 600)
+plot(rand, vertex.size = 2, vertex.label = NA)
+
+## 1b) Barabási–Albert model + plot
+bara <- make_tree(1000, children = 3, mode = "undirected")
+plot(bara, vertex.size = 2, vertex.label = NA)
+
+## --------------------2--------------------
 
 ## 2) Matice sousednosti
 as_adjacency_matrix(rand)
@@ -26,5 +27,17 @@ mean_distance(bara)
 ## 2d) Stupnì jednotlivých vrcholù + max stupeò
 degree(rand)
 max(degree(rand))
+
 degree(bara)
 max(degree(bara))
+
+## --------------------3--------------------
+
+## 3a) Cilené odebární maximálního stupnì
+subgraph_maxdegree_rand_1 = delete_vertices(rand, max(degree(rand)))
+subgraph_maxdegree_bara_1 = delete_vertices(bara, max(degree(bara)))
+
+## 3a) Cilené odebární nahodného stupnì
+subgraph_random_rand_1 = delete_vertices(rand, 3)
+subgraph_random_bara_1 = delete_vertices(bara, 3)
+
